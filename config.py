8c2575b1f9a44291993f3db9da880cfd4d344e33
SeekerC137 <volkov-a-s@yandex.ru>
2022-10-03 23:47:34 +0500

    fly.io service deployment commit

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 DB_URI = os.environ.get('DATABASE_URL')
 
-RSS_LIST = []
+RSS_LIST = [
+    "https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru",
+    "https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en",
+]
